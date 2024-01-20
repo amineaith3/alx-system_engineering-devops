7ce8a165108540944c5c2415a76dd23d181abcf4
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-20 12:22:30 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# install_flask.pp
-
-package { 'python3-pip':
-  ensure => installed,
-}
+# install_flask
 
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
 }
-
