716573ec7f0429f9beb0d5aa106167055f6946f4
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-20 12:20:33 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# create_file.pp
+# creates a file in /tmp
 
 file { '/tmp/school':
-  ensure   => 'file',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
-
