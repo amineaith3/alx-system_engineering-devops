94be0e78ea537a9a129ba1544947beb8e37b27d6
amineaith3 <amineaithamma2004@gmail.com>
2024-08-30 09:29:11 +0100

    bruh

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,29 +2,48 @@
 """For a given employee ID, returns information about
 their TODO list progress"""
 
-import requests
-import sys
+def get_employee_todo_progress(employee_id):
+    """Fetch and display employee's TODO list progress."""
+    # API URLs
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?
+    userId={employee_id}"
+
+    # Fetch employee data
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print(f"User with ID {employee_id} not found.")
+        return
+    employee = user_response.json()
+    employee_name = employee.get('name').strip()
+
+    # Fetch TODO list data
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    # Calculate TODO progress
+    total_tasks = len(todos)
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    number_of_done_tasks = len(completed_tasks)
+
+    # Print the TODO progress
+    print(
+        f"Employee {employee_name} is done with tasks"
+        f"({number_of_done_tasks}/{total_tasks}):"
+    )
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
 
-    name = user.json().get('name')
-
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
-    completed = 0
-
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
-
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
+    get_employee_todo_progress(employee_id)
